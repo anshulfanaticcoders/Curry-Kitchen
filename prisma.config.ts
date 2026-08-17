6525e6b92e81ef2a62c5d90ce2df348d581b22ad
anshulfanaticcoders <anshul@fanaticcoders.com>
2026-08-18 01:22:01 +0530

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "@prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL ?? "mysql://root:@localhost:3306/currykitchen_next",
+    url: env("DATABASE_URL"),
   },
   migrations: {
     path: "prisma/migrations",
